4b511876679f03bcb6638592c7f63f9d1531f5cd
EduardoZ-dev <eduardozequeira0@gmail.com>
2025-03-19 10:36:44 -0500

    creacion base del projecto y capa dominio

---
+++ Domain/Customers/Customer.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Primitives;
+using Domain.ValueObjects;
+using System.Net;
+
+namespace Domain.Customers
+{
+    public sealed class Customer : AggregateRoot
+    {
+        public Customer(CustomerId id, string name, string lastName, string email, PhoneNumber phoneNumber, Address address, bool active)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Address = address;
+            Active = active;
+        }
+
+        private Customer()
+        {
+
+        }
+
+        public CustomerId Id { get; private set; }
+        public string Name { get; private set; }
+        public string LastName { get; set; } = null!;
+        public string FullName => $"{Name} {LastName}";
+        public string Email { get; private set; } = null!;
+        public PhoneNumber PhoneNumber { get; private set; }
+        public Address Address { get; private set; }
+        public bool Active { get; private set; }
+
+        public static Customer UpdateCustomer(Guid id, string name, string lastName, string email, PhoneNumber phoneNumber, Address address, bool active)
+        {
+            return new Customer(new CustomerId(id), name, lastName, email, phoneNumber, address, active);
+        }
+    }
+}
